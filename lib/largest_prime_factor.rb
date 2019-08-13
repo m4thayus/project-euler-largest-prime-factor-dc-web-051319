4070e72fb09528cd323bc91396ff3fd272dacefc
Matt Williams <matt.seongjun@gmail.com>
2019-08-13 09:42:22 -0400

    Need to figure out an optimized version

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,48 @@
 # Enter your procedural solution here!
+def largest_prime_factor(n)
+    fact = n - 1
+    while fact > 0
+        if n % fact == 0
+            puts fact
+            prime = true
+            fact_of_fact = fact - 1
+            while fact_of_fact > 1
+                if fact % fact_of_fact == 0
+                    prime = false
+                end
+                fact_of_fact -= 1
+            end
+            if prime == true
+                return fact
+            end
+        end
+        fact -= 1
+    end
+end
+
+def optimized_largest_prime_factor(n)
+    fact = n - 1
+    while fact > 0
+        if n % fact == 0
+            prime = true
+            fact_of_fact = fact - 1
+            while fact_of_fact > 1
+                if fact % fact_of_fact == 0
+                    prime = false
+                end
+                fact_of_fact -= 1
+            end
+            if prime == true
+                return fact
+            end
+        end
+        fact -= 1
+    end
+end
+
+puts largest_prime_factor(13195)
+puts largest_prime_factor(1000000)
+puts largest_prime_factor(600851475143)
+# puts optimized_largest_prime_factor(600851475143)
+
+
